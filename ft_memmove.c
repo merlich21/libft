8ba8d3837a6ffb0fe0d43f40bed6a8fa07ced3a8
Marylin Erlich <alexandsko@gmail.com>
2021-10-15 21:17:19 +0300

    ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,50 +6,37 @@
 /*   By: merlich <merlich@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 13:37:37 by merlich           #+#    #+#             */
-/*   Updated: 2021/10/14 16:50:11 by merlich          ###   ########.fr       */
+/*   Updated: 2021/10/15 21:14:09 by merlich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t		i;
-	const char	*src1;
-	char		*dest1;
-	char		*tmp;
+	size_t		start;
+	size_t		stop;
+	int			step;
 
-	i = 0;
-	src1 = (char *) src;
-	dest1 = (char *) dest;
-	while (i < n)
+	if (dest != src)
 	{
-		tmp[i] = src1[i];
-		i++;
-	}
-	i = 0;
-	while (i < n)
-	{
-		dest1[i] = tmp[i];
-		i++;
+		if (dest > src)
+		{
+			start = n - 1;
+			step = -1;
+			stop = -1;
+		}
+		if (dest < src)
+		{
+			start = 0;
+			step = 1;
+			stop = n;
+		}
+		while (start != stop)
+		{
+			((char *)(dest))[start] = ((const char *)(src))[start];
+			start += step;
+		}
 	}
 	return (dest);
-}
-
-int	main(void)
-{
-	size_t		n;
-	const void	*src;
-	void		*dest;
-	const char	str[] = "zzzzzzzzz";
-	char		str1[] = "OOOOOPPPPPP&";
-
-	n = 1;
-	src = &str;
-	dest = &str1;
-	printf("%s\n", ft_memmove(dest, src, n));
-	//printf("%s\n", memmove(dest, src, n));
-	return (0);
 }
