25aaec37514fb837fdfb198e658ce5ac345b5484
Marylin Erlich <alexandsko@gmail.com>
2021-10-20 18:00:52 +0300

    atoi.c 00000000000000000000000042

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: merlich <merlich@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 20:34:31 by merlich           #+#    #+#             */
-/*   Updated: 2021/10/19 23:30:58 by merlich          ###   ########.fr       */
+/*   Updated: 2021/10/20 17:49:41 by merlich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,16 @@ static void	ft_issign(const char *str, int *i, int *flag_sign)
 	return ;
 }
 
-int	ft_atoi(const char *nptr)
+static void	ft_iszero(const char *str, int *i)
+{
+	while (str[*i] == '0')
+	{
+		*i = *i + 1;
+	}
+	return ;
+}
+
+int	ft_atoi(const char *str)
 {
 	int			i;
 	int			j;
@@ -46,16 +55,17 @@ int	ft_atoi(const char *nptr)
 	j = 0;
 	flag_sign = 1;
 	res = 0;
-	ft_isspace(nptr, &i);
-	ft_issign(nptr, &i, &flag_sign);
-	while (nptr[i] >= '0' && nptr[i] <= '9')
+	ft_isspace(str, &i);
+	ft_issign(str, &i, &flag_sign);
+	ft_iszero(str, &i);
+	while (str[i] >= '0' && str[i] <= '9')
 	{
 		j++;
 		if ((j >= 19) && (flag_sign == 1))
 			return (-1);
 		if ((j >= 19) && (flag_sign == -1))
 			return (0);
-		res = res * 10 + (nptr[i] - '0');
+		res = res * 10 + (str[i] - '0');
 		i = i + 1;
 	}
 	return ((int)(res * flag_sign));
