aa9332dbb2819964d13626dbbe7d9adf25b4a51a
Marylin Erlich <alexandsko@gmail.com>
2021-10-26 14:37:59 +0300

    ft_lstmap_bonus.c is OK

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: merlich <merlich@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 16:07:19 by merlich           #+#    #+#             */
-/*   Updated: 2021/10/24 19:14:19 by merlich          ###   ########.fr       */
+/*   Updated: 2021/10/26 14:04:11 by merlich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	while (lst != NULL && lst->next != NULL)
+	if (lst != NULL)
 	{
-		lst = lst->next;
+		while (lst->next != NULL)
+		{
+			lst = lst->next;
+		}
 	}
 	return (lst);
 }
--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: merlich <merlich@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 17:07:15 by merlich           #+#    #+#             */
-/*   Updated: 2021/10/25 23:26:21 by merlich          ###   ########.fr       */
+/*   Updated: 2021/10/26 14:34:36 by merlich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,40 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	**new_list;
+	t_list	*new_list;
 	t_list	*new_elem;
 	t_list	*tmp;
 
-	new_elem = ft_lstnew(f(lst->content));
-	if (NULL == new_elem)
+	if ((NULL != f) && (NULL != del))
 	{
-		ft_lstdelone(new_elem, del);
-		return (NULL);
-	}
-	new_list = &new_elem;
-	tmp = new_elem;
-	
-	while (lst)
-	{
-		lst = lst->next;
 		new_elem = ft_lstnew(f(lst->content));
-		if (new_elem != NULL)
+		if (NULL == new_elem)
 		{
-			ft_lstadd_back(&tmp, new_elem);
-			tmp = new_elem;
+			ft_lstdelone(new_elem, del);
+			return (NULL);
 		}
-		else
+		new_list = new_elem;
+		tmp = new_elem;
+		lst = lst->next;
+		while (lst)
 		{
-			ft_lstclear(new_list, del);
-			break ;
+			new_elem = ft_lstnew(f(lst->content));
+			if (new_elem != NULL)
+			{
+				ft_lstadd_back(&tmp, new_elem);
+				tmp = new_elem;
+				lst = lst->next;
+			}
+			else
+			{
+				ft_lstclear(&new_list, del);
+				break ;
+			}
 		}
+		return (new_list);
 	}
-	return (*new_list);
+	else
+	{
+		return (lst);
+	}
 }
